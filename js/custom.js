674b16e71d20e263277a3e01187113e827a00afa
gwanwoodev <gwanwoodev@kakao.com>
2020-10-16 01:57:40 +0900

    update services

---
+++ js/custom.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+    document.querySelector("#button-addon2").addEventListener("click", function(event) {
+        alert("아직 구현되지 않은 기능입니다.");
+    });
+});
